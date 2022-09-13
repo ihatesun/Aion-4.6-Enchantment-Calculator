83882f42c189b7d78f9faf83257ba79b240febdb
Milan Skendzic-Vulic <milanskendzicvulic@gmail.com>
2022-09-13 14:51:20 +0200

    added heroic and fable

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
-let itemLevel = document.getElementById("slItemLevel");
-let itemEnchantmentLevel = document.getElementById("slEnchLevel");
-let itemType = document.querySelector('input[name="item-type"]:checked');
+const itemLevel = document.getElementById("slItemLevel");
+const itemEnchantmentLevel = document.getElementById("slEnchLevel");
 
-let table = document.getElementById("calculator-output-content");
-let itemTypeRadios = document.querySelectorAll('input[name="item-type"]');
+const table = document.querySelector(".calculator-output__list");
+const itemTypeRadios = document.querySelectorAll('input[name="item-type"]');
 
 const MAX_ITEM_LEVEL = 65;
 const MAX_ENCH_LEVEL = 15;
 
 for (let i = MAX_ITEM_LEVEL; i > 0; i--) {
-    itemLevel.innerHTML += `<option value="${i}">${i}</option>`;
+  itemLevel.innerHTML += `<option value="${i}">${i}</option>`;
 }
 
 for (let i = 0; i < MAX_ENCH_LEVEL; i++) {
-    itemEnchantmentLevel.innerHTML += `<option value="${i}">${i}</option>`;
+  itemEnchantmentLevel.innerHTML += `<option value="${i}">${i}</option>`;
 }
 
 generateTable();
 
 document.getElementById("slEnchLevel").addEventListener("change", () => {
-    generateTable();
+  generateTable();
 });
 
 document.getElementById("slItemLevel").addEventListener("change", () => {
-    generateTable();
+  generateTable();
 });
 
 for (const radioButton of itemTypeRadios) {
-    radioButton.addEventListener('change', () => {
-        itemType = document.querySelector('input[name="item-type"]:checked');
+  radioButton.addEventListener("change", () => {
+    itemLevel.firstElementChild.selected = true;
+    itemEnchantmentLevel.firstElementChild.selected = true;
 
-        itemLevel.firstElementChild.selected = true;
-        itemEnchantmentLevel.firstElementChild.selected = true;
-
-        generateTable();
-    });
+    generateTable();
+  });
 }
 
 function generateTable() {
-    table.innerHTML = "";
+  table.innerHTML = "";
 
-    maxStone =
-        Number(itemLevel.value) + Number(itemType.value) + Number(itemEnchantmentLevel.value);
+  const itemType = document.querySelector('input[name="item-type"]:checked');
 
-    for (let difference = 40; difference >= 0; difference--) {
-        table.innerHTML += `<div class="calculator-output-content-row">
+  maxStone = Number(itemLevel.value) + Number(itemType.value) + Number(itemEnchantmentLevel.value);
+
+  for (let difference = 40; difference >= 0; difference--) {
+    table.innerHTML += `<div class="calculator-output__list-item">
                                 <div>L${maxStone--}</div>
                                 <div>${difference * 2}%</div>
                             </div>`;
-    }
+  }
 }
